3c64b6ea674de0b5b636afe07b7af0538fc0d6fa
hasan <43574156+mdhasanali3@users.noreply.github.com>
2023-09-02 19:34:04 +0600

    Changed c2f with SPDconv

--- nn/modules.py
+++ nn/modules.py
@@ -195,17 +195,25 @@ class C2(nn.Module):
 
 class C2f(nn.Module):
     # CSP Bottleneck with 2 convolutions
-    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+    # def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+    #     super().__init__()
+    #     self.c = int(c2 * e)  # hidden channels
+    #     self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+    #     self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+    #     self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    # def forward(self, x):
+    #     y = list(self.cv1(x).split((self.c, self.c), 1))
+    #     y.extend(m(y[-1]) for m in self.m)
+    #     return self.cv2(torch.cat(y, 1))
+
+    # Changing the dimension of the Tensor basically SPD code calling with c2f for testing purpose only
+    def __init__(self, dimension=1):
         super().__init__()
-        self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.d = dimension
 
     def forward(self, x):
-        y = list(self.cv1(x).split((self.c, self.c), 1))
-        y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(torch.cat(y, 1))
+        return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
 
 
 class ChannelAttention(nn.Module):
